672c63cfe5ddf663f7c5494b6bb2d02b0de064d0
Roman Belyaev <rsbel94@ya.ru>
2025-03-25 12:33:22 +0300

    refactirong and optimization

--- src/PolynomProcessor.hpp
+++ src/PolynomProcessor.hpp
@@ -22,7 +22,8 @@ private:
     PolynomProcessor& operator=(const PolynomProcessor&) = delete;
     PolynomProcessor&& operator=(PolynomProcessor&&) noexcept = delete;
 
-    inline size_t find_max_key();
+    inline size_t define_max_iterations();
+    inline bool parse_polynoms();
 
 public:
     explicit PolynomProcessor(BinaryOperator&& op);
@@ -31,40 +32,61 @@ public:
     std::map<int, int> operator()();
 };
 
+/*
+    Constructor.
+*/
 template <class BinaryOperator>
 PolynomProcessor<BinaryOperator>::PolynomProcessor(BinaryOperator&& op)
     : op(std::forward<BinaryOperator>(op)), polynom1(parser.get_polynom1()),
       polynom2(parser.get_polynom2()){};
 
+/*
+    Return number of necessary iterations for processing polynoms.
+*/
 template <class BinaryOperator>
-inline size_t PolynomProcessor<BinaryOperator>::find_max_key()
+inline size_t PolynomProcessor<BinaryOperator>::define_max_iterations()
 {
     return std::max((*(polynom1.crbegin())).first,
                     (*(polynom2.crbegin())).first);
 }
 
+/*
+    Try to parse polynoms.
+*/
+template <class BinaryOperator>
+inline bool PolynomProcessor<BinaryOperator>::parse_polynoms()
+{
+    try
+    {
+        parser.parse();
+        return true;
+    }
+    catch (const std::exception& ex)
+    {
+        std::cerr << ex.what();
+        return false;
+    }
+}
+
+/*
+    Main function of processing polynoms.
+*/
 template <class BinaryOperator>
 std::map<int, int> PolynomProcessor<BinaryOperator>::operator()()
 {
     if (!result_polynom.empty())
         return result_polynom;
 
-    try
+    if (parse_polynoms())
     {
-        parser.parse();
+        size_t iterations = define_max_iterations();
+
+        for (size_t i{0}; i <= iterations; ++i)
+            result_polynom[i] = op(polynom1[i], polynom2[i]);
+
+        return result_polynom;
     }
-    catch (const std::exception& ex)
-    {
-        std::cerr << ex.what();
-        return {};
-    }
-
-    size_t iterations = find_max_key();
-
-    for (size_t i{0}; i <= iterations; ++i)
-        result_polynom[i] = op(polynom1[i], polynom2[i]);
-
-    return result_polynom;
+    return {};
 }
 
 #define POLYNOM_PROCESSOR_H
