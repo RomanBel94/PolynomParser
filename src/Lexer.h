22b39deb4570bdd0b68eee4c3d164ad09fc291cb
Roman Belyaev <rsbel94@ya.ru>
2025-03-28 12:28:42 +0300

    moved Token to a separate file

--- src/Lexer.h
+++ src/Lexer.h
@@ -5,54 +5,10 @@
 #include <regex>
 #include <sstream>
 
+#include "Token.hpp"
+
 namespace LexerParser
 {
-/*
-    Types of used tokens
-*/
-enum class TokenType : unsigned char
-{
-    Undefined,
-    Number,
-    Newline,
-    Eof
-};
-
-/*
-    Struct for token description
-*/
-struct Token
-{
-    const int value;
-    const TokenType type;
-
-    explicit Token(const TokenType type, const short value = '\0')
-        : type(type), value(value)
-    {
-    }
-    ~Token() = default;
-
-    inline bool Token::operator==(TokenType rhs) const
-    {
-        return this->type == rhs;
-    }
-
-    inline bool Token::operator!=(TokenType rhs) const
-    {
-        return this->type != rhs;
-    }
-
-    inline Token::operator TokenType() const { return type; }
-    inline Token::operator int() const { return value; }
-
-private:
-    Token() = delete;
-    Token(const Token&) = delete;
-    Token(Token&&) noexcept = delete;
-    const Token& operator=(const Token&) = delete;
-    Token&& operator=(Token&&) noexcept = delete;
-};
-
 /*
     Lexer extracts tokens from input file
 */
